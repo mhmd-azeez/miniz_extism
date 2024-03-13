aec54cfdcf214f90ee978ed8d0ae8db59332f5de
Muhammad Azeez <muhammad@dylibso.com>
2024-03-13 19:44:58 +0300

    add go host

---
+++ host/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	extism "github.com/extism/go-sdk"
+)
+
+func main() {
+	manifest := extism.Manifest{
+		Wasm: []extism.Wasm{
+			extism.WasmFile{
+				Path: "../plugin/build/miniz_extism.wasm",
+			},
+		},
+	}
+
+	ctx := context.Background()
+	config := extism.PluginConfig{
+		EnableWasi: true,
+	}
+	plugin, err := extism.NewPlugin(ctx, manifest, config, []extism.HostFunction{})
+
+	if err != nil {
+		fmt.Printf("Failed to initialize plugin: %v\n", err)
+		os.Exit(1)
+	}
+
+	defer plugin.Close()
+
+	data := []byte("Hello, World!")
+
+	_, out, err := plugin.Call("compress_stream", data)
+	if err != nil {
+		fmt.Printf("Failed to call compress_stream: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Compressed data len: %v\n", len(out))
+
+	_, out, err = plugin.Call("decompress_stream", out)
+	if err != nil {
+		fmt.Printf("Failed to call decompress_stream: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Decompressed data: %s\n", string(out))
+}
